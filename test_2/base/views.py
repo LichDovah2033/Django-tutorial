4550a6f3c79cbe69cacd26d58df7e9f16d28f2d3
LichDovah2033 <dragondrake02@outlook.com>
2025-02-09 18:22:46 -0500

    removed massive code from home template
    modified files slightly, still cant get the templates to work

--- test_2/base/views.py
+++ test_2/base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
 
+# Create your views here.
+# how do I use the tempalets properly?????
 
 def home(request):
     return render(request, 'home.html')
